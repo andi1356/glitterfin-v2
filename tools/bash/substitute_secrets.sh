97e9b959c456c85be60665dfa3191e3d0a2e72ae
Robu Andrei <robu.andrew@gmail.com>
2025-06-30 10:23:08 -0400

    t14

--- tools/bash/substitute_secrets.sh
+++ tools/bash/substitute_secrets.sh
@@ -1,1 +1,1 @@
-export $(cat secrets | xargs) && for file in $(cat code_files); do envsubst < "$file" > "$file.tmp" && mv "$file.tmp" "$file"; done
+openssl enc -aes-256-cbc -pbkdf2 -iter 1000 -salt -in secrets -out secrets.enc -pass "file:tools/enc_password_file"
