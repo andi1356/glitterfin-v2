70037baa2cb7b9be288cc562a76a6bfd877e7a72
Robu Andrei <robu.andrew@gmail.com>
2025-06-30 09:10:39 -0400

    first commit

---
+++ src/main/java/arobu/glitterfinv2/GlitterfinV2Application.java
@@ -0,0 +1,13 @@
+package arobu.glitterfinv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlitterfinV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlitterfinV2Application.class, args);
+    }
+
+}
