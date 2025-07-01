4f74ae91d31600f5f8f9a5e73f8704eb7741ed58
Robu Andrei <robu.andrew@gmail.com>
2025-07-01 00:24:42 -0400

    207

--- src/main/java/arobu/glitterfinv2/TestEndpoint.java
+++ src/main/java/arobu/glitterfinv2/TestEndpoint.java
@@ -1,11 +1,12 @@
 package arobu.glitterfinv2;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
 public class TestEndpoint {
+    @GetMapping("test")
     public String get(){
         return "Hello World";
     }
