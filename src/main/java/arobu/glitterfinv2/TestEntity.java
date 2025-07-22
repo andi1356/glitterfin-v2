1821784b27a8913ca5d6b9a057ee5279c68e1f2e
Robu Andrei <robu.andrew@gmail.com>
2025-07-22 11:21:32 -0400

    POC thymeleaf

--- src/main/java/arobu/glitterfinv2/TestEntity.java
+++ src/main/java/arobu/glitterfinv2/TestEntity.java
@@ -12,11 +12,24 @@ public class TestEntity {
     public TestEntity() {
     }
 
+    public TestEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
---
+++ src/main/java/arobu/glitterfinv2/controller/frontend/HomeController.java
@@ -0,0 +1,24 @@
+package arobu.glitterfinv2.controller.frontend;
+
+import arobu.glitterfinv2.TestEntity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("appName", "Glitterfin v2");
+        model.addAttribute("message", "Welcome to your Spring Boot app!");
+        SecurityProperties.User user = new SecurityProperties.User();
+        user.setName("Andrei");
+        model.addAttribute("user", user);
+        model.addAttribute("items", List.of(new TestEntity(1L,"Joke"), new TestEntity(2L, "Another Joke")));
+        return "index"; // returns templates/index.html
+    }
+}
