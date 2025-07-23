325b097e77db5beaeba3281f6b6059c1e74579ec
Robu Andrei <robu.andrew@gmail.com>
2025-07-23 01:24:43 -0400

    update encryption from binary to base64

--- tools/bash/decrypt_secrets.sh
+++ tools/bash/decrypt_secrets.sh
@@ -1,1 +1,1 @@
-openssl enc -d -aes-256-cbc -pbkdf2 -iter 1000 -salt -in .env.prod.enc -out .env.prod -pass "file:tools/enc_password_file"
+openssl enc -d -aes-256-cbc -base64 -pbkdf2 -iter 1000 -salt -in .env.prod.enc -out .env.prod -pass "file:tools/enc_password_file"
--- tools/bash/encrypt_secrets.sh
+++ tools/bash/encrypt_secrets.sh
@@ -1,2 +1,2 @@
-openssl enc -aes-256-cbc -pbkdf2 -iter 1000 -salt -in .env.prod -out .env.prod.enc -pass "file:tools/enc_password_file"
+openssl enc -aes-256-cbc -base64 -pbkdf2 -iter 1000 -salt -in .env.prod -out .env.prod.enc -pass "file:tools/enc_password_file"
 
