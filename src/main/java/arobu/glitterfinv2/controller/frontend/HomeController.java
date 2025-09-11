8bd184e42473805567ab82eb59c70361cd287dac
Robu Andrei <robu.andrew@gmail.com>
2025-09-11 15:01:41 -0400

    Update user id to be the username

--- src/main/java/arobu/glitterfinv2/controller/frontend/HomeController.java
+++ src/main/java/arobu/glitterfinv2/controller/frontend/HomeController.java
@@ -1,24 +1,22 @@
 package arobu.glitterfinv2.controller.frontend;
 
-import arobu.glitterfinv2.model.entity.TestEntity;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("appName", "Glitterfin v2");
-        model.addAttribute("message", "Welcome to your Spring Boot app!");
-        SecurityProperties.User user = new SecurityProperties.User();
-        user.setName("Andrei");
-        model.addAttribute("user", user);
-        model.addAttribute("items", List.of(new TestEntity(1L,"Joke"), new TestEntity(2L, "Another Joke")));
+        model.addAttribute("appName", "Glitterfin");
+        model.addAttribute("message", "Glitterfin! Finances done with glitter");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        model.addAttribute("user", authentication);
         return "index"; // returns templates/index.html
     }
 }
