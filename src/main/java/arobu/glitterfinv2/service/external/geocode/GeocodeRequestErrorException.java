c3b935efd763e6de197676a502119e966d1223cc
Robu Andrei <robu.andrew@gmail.com>
2025-09-10 20:03:25 -0400

    Update Location entity to include separate address columns

--- src/main/java/arobu/glitterfinv2/service/external/geocode/GeocodeRequestErrorException.java
+++
@@ -1,9 +0,0 @@
-package arobu.glitterfinv2.service.external.geocode;
-
-import java.net.http.HttpResponse;
-
-public class GeocodeRequestErrorException extends Throwable {
-    public GeocodeRequestErrorException(HttpResponse<String> geocodingResponseString) {
-        super(geocodingResponseString.toString());
-    }
-}
